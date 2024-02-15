06cd683061f2e275dfce99adab77a37c8ccb3fe6
Brayan1951 <brayanc1951@gmail.com>
2024-02-14 22:15:43 -0500

    agregando texto part1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,15 @@ import './component.css'
 export default function Navbar() {
   return (
  <nav>
+      <div className='logo'>
+        <h1>City Frut</h1>
+      </div>
+      <div className='enlaces'>
+
       <NavLink to={"/"}>Inicio</NavLink>
       <NavLink to={"/carta"}>Carta</NavLink>
       <NavLink to={"/conocenos"}>Conocenos</NavLink>
+      </div>
     
 
  </nav>
--- src/pages/InicioPage.jsx
+++ src/pages/InicioPage.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
 
+import cremoladas from './../assets/tamaños_2.jpg'
+
 export default function InicioPage() {
   return (
-    <div>InicioPage</div>
+
+    <main>
+      {/* Introduccion */}
+      <section>
+        <h1>CREMOLADAS DE PURA HECHOS CON EL SABOR PERUANO</h1>
+        <article>
+
+        <p>El amor de Okasan es el secreto de nuestras delicias. Nuestros postres japoneses representan el corazón y el alma de la tradición culinaria japonesa</p>
+        <img src={cremoladas} alt="" />
+        </article>
+      </section>
+
+      {/* Nuevos Sabores */}
+      <section>
+        <h1>Nuenos Sabores</h1>
+      </section>
+      {/* Testimonios */}
+      <section>
+        <h1>Testimonios</h1>
+      </section>
+
+
+    </main>
   )
 }
