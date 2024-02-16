5b53f778b1c720a201edba9e61ff35973727666c
Brayan1951 <brayanc1951@gmail.com>
2024-02-15 21:57:17 -0500

    completando home page

--- src/pages/InicioPage.jsx
+++ src/pages/InicioPage.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 import cremoladas from './../assets/tamaños_2.jpg'
+import lucuma from './../assets/LUCUMA.png'
+import mango from './../assets/mango.jpg'
+import testimonio from './../assets/apoyo.jpg'
 
 export default function InicioPage() {
   return (
@@ -11,18 +14,36 @@ export default function InicioPage() {
         <h1>CREMOLADAS DE PURA HECHOS CON EL SABOR PERUANO</h1>
         <article>
 
-        <p>El amor de Okasan es el secreto de nuestras delicias. Nuestros postres japoneses representan el corazón y el alma de la tradición culinaria japonesa</p>
-        <img src={cremoladas} alt="" />
+          <p>El amor de Okasan es el secreto de nuestras delicias. Nuestros postres japoneses representan el corazón y el alma de la tradición culinaria japonesa</p>
+          <img src={cremoladas} alt="" />
         </article>
       </section>
 
       {/* Nuevos Sabores */}
       <section>
-        <h1>Nuenos Sabores</h1>
+        <h1>Nuevos Sabores</h1>
+        <div className='sabores'>
+
+          <figure>
+            <img src={mango} alt="" />
+            {/* <img src={lucuma} alt="" /> */}
+            <figcaption>Sabor Lucuma</figcaption>
+          </figure>
+          <figure>
+            <img src={mango} alt="" />
+            <figcaption>Sabor Lucuma</figcaption>
+          </figure>
+        </div>
+
       </section>
       {/* Testimonios */}
-      <section>
+      <section className='testimoniio'>
         <h1>Testimonios</h1>
+        <article>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quas. Rem ducimus consequuntur non molestias dolore voluptatem laborum fugit, optio eum id corporis dolor dignissimos sequi maiores commodi ex odit.</p>
+
+          <img src={testimonio} alt="" />
+        </article>
       </section>
 
 
