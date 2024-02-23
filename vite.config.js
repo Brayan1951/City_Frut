42bb10b81f1e4e6a51bac50e2154947e31203daa
Brayan1951 <brayanc1951@gmail.com>
2024-02-22 22:50:25 -0500

    fixed 2.0

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  build: {
+    rollupOptions:{
+      output:{
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+
+    },
+    // Desactiva la generación de hashes para los nombres de archivo
+    // de las imágenes
+    assetsInlineLimit: 0,
+    assetFileNames: 'assets/[name].[ext]' 
+  }
 })
